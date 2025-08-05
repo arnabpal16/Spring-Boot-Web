2059e518431479e7fc16e025fe5c20b38dfc3023
Arnab Pal <arnab.pal.21@aot.edu.in>
2025-08-06 01:21:02 +0530

    Add EmployeeDTO and new employee endpoints
    Introduced EmployeeDTO to represent employee data. Updated EmployeeController to add endpoints for fetching employee by ID and listing employees with query parameters, replacing the previous secret message endpoint.

--- src/main/java/com/arnab/springbootwebpractice/springbootwebpractice/controllers/EmployeeController.java
+++ src/main/java/com/arnab/springbootwebpractice/springbootwebpractice/controllers/EmployeeController.java
@@ -1,15 +1,32 @@
 package com.arnab.springbootwebpractice.springbootwebpractice.controllers;
 
 
+import com.arnab.springbootwebpractice.springbootwebpractice.dto.EmployeeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class EmployeeController {
 
-    @GetMapping(path = "/getSecretMessage")
-    public String getMySuperSecretMessage() {
-        return "Secret Message -> Indian Army";
+//    @GetMapping(path = "/getSecretMessage")
+//    public String getMySuperSecretMessage() {
+//        return "Secret Message -> Indian Army";
+//    }
+
+    @GetMapping(path = "/employees/{employeeId}")
+    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId){
+        return new  EmployeeDTO(employeeId, "Arnab", "arnab@gmail.com", 23, LocalDate.of(2025, 8, 6), true);
+    }
+
+    @GetMapping(path = "/employees")
+    public String getEmployees(@RequestParam Integer age,@RequestParam(required = false)String sortBy){
+        return ("Arnab -> "+ age +" "+ sortBy);
     }
 
 }
