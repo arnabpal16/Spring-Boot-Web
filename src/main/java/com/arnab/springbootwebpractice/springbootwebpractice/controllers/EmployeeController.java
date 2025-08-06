843818bcd7c242f5773a36387dd55dc47c724658
Arnab Pal <arnab.pal.21@aot.edu.in>
2025-08-07 00:08:35 +0530

    created post/put mapping endpoint

--- src/main/java/com/arnab/springbootwebpractice/springbootwebpractice/controllers/EmployeeController.java
+++ src/main/java/com/arnab/springbootwebpractice/springbootwebpractice/controllers/EmployeeController.java
@@ -2,16 +2,14 @@ package com.arnab.springbootwebpractice.springbootwebpractice.controllers;
 
 
 import com.arnab.springbootwebpractice.springbootwebpractice.dto.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Locale;
 
 @RestController
+@RequestMapping(path = "/employees")
 public class EmployeeController {
 
 //    @GetMapping(path = "/getSecretMessage")
@@ -19,14 +17,23 @@ public class EmployeeController {
 //        return "Secret Message -> Indian Army";
 //    }
 
-    @GetMapping(path = "/employees/{employeeId}")
+    @GetMapping(path = "/{employeeId}")
     public EmployeeDTO getEmployeeById(@PathVariable Long employeeId){
         return new  EmployeeDTO(employeeId, "Arnab", "arnab@gmail.com", 23, LocalDate.of(2025, 8, 6), true);
     }
 
-    @GetMapping(path = "/employees")
-    public String getEmployees(@RequestParam Integer age,@RequestParam(required = false)String sortBy){
+    @GetMapping
+    public String getAllEmployees(@RequestParam(required = false) Integer age,@RequestParam(required = false)String sortBy){
         return ("Arnab -> "+ age +" "+ sortBy);
+    }
+
+    @PostMapping
+    public String createNewEmployee(){
+        return "Hello Post";
+    }
+
+    @PutMapping String updateEmployeeId(){
+        return "Hello Put";
     }
 
 }
